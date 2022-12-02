119a1918cd699655d62afb58588a15eaab515090
sandro <sandrosilva9029@gmail.com>
2022-12-02 13:50:32 -0300

    buscando dados dos alunos

--- frontend/src/pages/Alunos/styled.js
+++ frontend/src/pages/Alunos/styled.js
@@ -1,3 +1,23 @@
 import styled from 'styled-components';
 
-export const Paragrafo = styled.p``;
+export const AlunoContainer = styled.div`
+  margin-top: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
+  }
+
+  div + div {
+    border-top: 1px solid #000;
+  }
+`;
+
+export const ProfilePicture = styled.div`
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+`;
